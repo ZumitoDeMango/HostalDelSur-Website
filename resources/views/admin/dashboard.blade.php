bf8d7a182227d9292a9739e883c8be6bd374dbdc
Nayib Hazbun <nayibhp@gmail.com>
2024-09-29 02:42:28 -0300

    dashboard cambios menores

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,30 @@
 @extends('templates.master')
 
 @section('main-content')
-
+<div class="row">
+    <div class="col-6">
+        <div class="card text-white bg-dark">
+            <div class="card-body">
+                <h4 class="card-title mb-3 text-center">Check-In y Check-Out</h4>
+                <h4 class="card-title mb-3 text-center">Resumen de ganancias</h4>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card text-white bg-dark">
+            <div class="card-body">
+                <h4 class="card-title mb-3 text-center">GESTIÓN</h4>
+                <div class="d-grid mb-3">
+                    <a class="btn btn-primary" href="#" role="button">HABITACIONES</a>
+                </div>
+                <div class="d-grid mb-3">
+                    <a class="btn btn-primary" href="#" role="button">RESERVAS</a>
+                </div>
+                <div class="d-grid">
+                    <a class="btn btn-primary" href="#" role="button">PAGOS</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -15,14 +15,14 @@
         <header>
             <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
                 <div class="container">
-                    <a class="navbar-brand" href="#">HOSTAL DEL SUR</a>
+                    <a class="navbar-brand" href="{{ route('home.index') }}">HOSTAL DEL SUR</a>
                     <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="collapsibleNavId">
                         <ul class="navbar-nav me-auto mt-2 mt-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" href="/" aria-current="page">INICIO
+                                <a class="nav-link active" href="{{ route('home.index') }}" aria-current="page">INICIO
                                     <span class="visually-hidden">(current)</span>
                                 </a>
                             </li>
@@ -43,7 +43,7 @@
                             <!-- Mostrar botón de logout si el usuario está autenticado -->
                             @if (Auth::check())
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">DASHBOARD</a>
+                                    <a class="nav-link" href="{{ route('admin.dashboard') }}">DASHBOARD</a>
                                 </li>
                                 <li class="nav-item">
                                     <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" style="display: none;">
